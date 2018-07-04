0ce86cda85bbf3afa37c0b466c8f34d70c5820bb
zhouyuandizyd <1063740308@qq.com>
2018-07-04 18:53:05 +0800

    车轮项目

---
+++ carriage/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+   
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
